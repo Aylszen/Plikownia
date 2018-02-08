dd4fe1121b2f0d7f492bd9ac48be8df06f42ae05
Aylszen <aylszen@gmail.com>
2018-02-08 23:45:56 +0100

    Added shortcut icon for website

--- Strona/home.php
+++ Strona/home.php
@@ -14,6 +14,11 @@
 <head>
 	<meta charset="utf-8" />
 	<title>Plikownia</title>
+	<link rel="icon" href="/img/ico/favicon.ico">
+    <link rel="stylesheet" href="css/index.css" type=text/css>
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
 
 <body>
--- Strona/index.php
+++ Strona/index.php
@@ -14,6 +14,7 @@ if ((isset($_SESSION['zalogowany'])) && ($_SESSION['zalogowany']==true))
   <head>
     <meta charset="utf-8" />
     <title>Plikownia</title>
+	<link rel="icon" href="/img/ico/favicon.ico">
     <link rel="stylesheet" href="css/index.css" type=text/css>
     <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
