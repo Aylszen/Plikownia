b89f8689d9680f5cbdfeeb869debb0df0d20323b
Aylszen <aylszen@gmail.com>
2018-02-11 15:32:46 +0100

    Folder tree added

--- MySQL/plikowniaDB.sql
+++ MySQL/plikowniaDB.sql
@@ -1,24 +1,86 @@
 -- phpMyAdmin SQL Dump
--- version 4.2.11
--- http://www.phpmyadmin.net
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Czas generowania: 28 Gru 2015, 18:26
--- Wersja serwera: 5.6.21
--- Wersja PHP: 5.6.3
+-- Czas generowania: 11 Lut 2018, 15:31
+-- Wersja serwera: 10.1.29-MariaDB
+-- Wersja PHP: 7.1.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
--- Baza danych: `osadnicy`
+-- Baza danych: `plikowniadb`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `menus`
+--
+
+CREATE TABLE `menus` (
+  `user` varchar(100) NOT NULL,
+  `id` int(11) NOT NULL,
+  `label` varchar(50) NOT NULL,
+  `link` varchar(100) NOT NULL,
+  `parent` int(11) NOT NULL DEFAULT '0',
+  `sort` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Zrzut danych tabeli `menus`
+--
+
+INSERT INTO `menus` (`user`, `id`, `label`, `link`, `parent`, `sort`) VALUES
+('test', 1, 'test', '#', 0, 0),
+('test', 2, 'Piki', '#', 1, NULL),
+('test', 3, 'Inne', '#', 1, NULL),
+('KrzysztofKita', 4, 'KrzysztofKita', '#', 0, NULL),
+('KrzysztofKita', 5, 'Pliki', '#', 4, NULL),
+('HimekoKuran', 7, 'HimekoKuran', '$', 0, NULL),
+('HimekoKuran', 8, 'Pliki', '#', 7, NULL),
+('HimekoKuran', 9, 'Wazne', '', 7, NULL),
+('MariuszCiupinski', 10, 'MariuszCiupinski', '', 0, NULL),
+('MariuszCiupinski', 11, 'Pliki', '#', 10, NULL),
+('MariuszCiupinski', 12, 'Takie tam', '#', 10, NULL),
+('AndrzejStrek', 13, 'AndrzejStrek', '', 0, NULL),
+('AndrzejStrek', 14, 'Tutorials', '', 13, NULL),
+('KrzysztofKita', 15, 'Projekt', '', 4, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `treeview_items`
+--
+
+CREATE TABLE `treeview_items` (
+  `id` int(11) NOT NULL,
+  `name` varchar(200) NOT NULL,
+  `title` varchar(200) NOT NULL,
+  `parent_id` varchar(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Zrzut danych tabeli `treeview_items`
+--
+
+INSERT INTO `treeview_items` (`id`, `name`, `title`, `parent_id`) VALUES
+(1, 'task1', 'task1title', '2'),
+(2, 'task2', 'task2title', '0'),
+(3, 'task3', 'task1title3', '0'),
+(4, 'task4', 'task2title4', '3'),
+(5, 'task4', 'task1title4', '3'),
+(6, 'task5', 'task2title5', '5');
 
 -- --------------------------------------------------------
 
@@ -26,11 +88,11 @@ SET time_zone = "+00:00";
 -- Struktura tabeli dla tabeli `uzytkownicy`
 --
 
-CREATE TABLE IF NOT EXISTS `uzytkownicy` (
+CREATE TABLE `uzytkownicy` (
   `id` int(11) NOT NULL,
   `user` text COLLATE utf8_polish_ci NOT NULL,
   `pass` text COLLATE utf8_polish_ci NOT NULL
-) ENGINE=MyISAM AUTO_INCREMENT=11 DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
 
 --
 -- Zrzut danych tabeli `uzytkownicy`
@@ -48,20 +110,47 @@ INSERT INTO `uzytkownicy` (`id`, `user`, `pass`) VALUES
 --
 
 --
+-- Indexes for table `menus`
+--
+ALTER TABLE `menus`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `treeview_items`
+--
+ALTER TABLE `treeview_items`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `uzytkownicy`
 --
 ALTER TABLE `uzytkownicy`
- ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `id` (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id` (`id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
+-- AUTO_INCREMENT dla tabeli `menus`
+--
+ALTER TABLE `menus`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+
+--
+-- AUTO_INCREMENT dla tabeli `treeview_items`
+--
+ALTER TABLE `treeview_items`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
 -- AUTO_INCREMENT dla tabeli `uzytkownicy`
 --
 ALTER TABLE `uzytkownicy`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=11;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- Strona/home.php
+++ Strona/home.php
@@ -1,33 +1,66 @@
 <?php
 
-	session_start();
-	
-	if (!isset($_SESSION['zalogowany']))
-	{
-		header('Location: index.php');
-		exit();
-	}
-	
+session_start();
+
+if (!isset($_SESSION['zalogowany']))
+{
+	header('Location: index.php');
+	exit();
+}
+
 ?>
 <!DOCTYPE HTML>
 <html lang="pl">
-<head>
-	<meta charset="utf-8" />
-	<title>Plikownia</title>
-	<link rel="icon" href="/img/ico/favicon.ico">
-    <link rel="stylesheet" href="css/index.css" type=text/css>
-    <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-</head>
+	<head>
+		<meta charset="utf-8" />
+		<title>Plikownia</title>
+		<link rel="icon" href="/img/ico/favicon.ico">
+		<link rel="stylesheet" href="css/home.css" type=text/css media="all">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	</head>
 
-<body>
-	
-<?php
+	<body>
+		<?php
 
-	echo "<p>Witaj ".$_SESSION['user'].'! [ <a href="logout.php">Wyloguj się!</a> ]</p>';
-	
-?>
+		echo "<p>Witaj <b>".$_SESSION['user'].'</b>! [ <a href="logout.php">Wyloguj się!</a> ]</p>';
 
-</body>
+		?>
+		<div class="container" role="main">
+
+			<div class="c1">c1</div>
+			<div class="c2"><?php 
+				require_once "connect.php";
+
+				$conn = @new mysqli($host, $db_user, $db_password, $db_name);
+				include_once("functions.php");
+				?>
+				<link rel="stylesheet" href="css/home.css" type="text/css" media="all">
+				<?php
+				$sql = "SELECT id, label, link, parent FROM menus WHERE user = '".$_SESSION['user']."' ORDER BY parent, sort, label ";
+				$result = mysqli_query($conn, $sql) or die("database error:". mysqli_error($conn));
+				// Create an array to conatin a list of items and parents
+				$menus = array(
+					'items' => array(),
+					'parents' => array()
+				);
+				// Builds the array lists with data from the SQL result
+				while ($items = mysqli_fetch_assoc($result)) {
+					// Create current menus item id into array
+					$menus['items'][$items['id']] = $items;
+					// Creates list of all items with children
+					$menus['parents'][$items['parent']][] = $items['id'];
+				}
+				// Print all tree view menus 
+				echo createTreeView(0, $menus);
+				?></div>
+			<div class="c3">c3 </div>
+		</div>
+
+
+
+		<footer id="STOPKA">Plikownia</footer>
+	</body>
 </html>
